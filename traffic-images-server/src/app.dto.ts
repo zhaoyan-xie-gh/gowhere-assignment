59fd2cac2da427b0309847b30de3d4e87b3c61b4
Zhaoyan <zhaoyan@watchTowr.com>
2023-09-19 18:09:28 +0800

    Implement global validation

--- traffic-images-server/src/app.dto.ts
+++ traffic-images-server/src/app.dto.ts
@@ -1,15 +1,33 @@
-export interface LatLong {
+import { Type } from 'class-transformer';
+import { IsNumber, IsString, ValidateNested } from 'class-validator';
+
+export class LatLong {
+  @IsNumber()
   latitude: number;
+
+  @IsNumber()
   longitude: number;
 }
-export interface AreaMetadata {
+
+export class AreaMetadata {
+  @IsString()
   name: string;
+
+  @ValidateNested()
+  @Type(() => LatLong)
   label_location: LatLong;
 }
-export interface ReverseGeoCodingDto {
+
+export class ReverseGeoCodingDto {
+  @ValidateNested({ each: true })
+  @Type(() => AreaMetadata)
   areaMetadata: AreaMetadata[];
+
+  @ValidateNested({ each: true })
+  @Type(() => LatLong)
   locations: LatLong[];
 }
+
 export interface LatLongWithName {
   name: string | null;
   location: LatLong;
